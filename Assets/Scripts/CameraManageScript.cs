109d71b4a01ba17eb9f0336ea2ddbf9b366f64cd
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-02 20:03:12 +0500

    Добавил возможность изменения масштаба с помощью колеса мыши

--- Assets/Scripts/CameraManageScript.cs
+++ Assets/Scripts/CameraManageScript.cs
@@ -1,10 +1,44 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using Slider = UnityEngine.UI.Slider;
 
 public class CameraManageScript : MonoBehaviour
 {
-	public void ChangeCameraSize()
-	{
-	    Camera.main.orthographicSize = 100 - gameObject.GetComponent<Slider>().value;
-	}
+    public Slider slider; 
+
+    public void ChangeCameraSize()
+    {
+        Camera.main.orthographicSize = 100 - slider.value;
+    }
+
+    void Update()
+    {
+        float axis = Input.GetAxis("Mouse ScrollWheel");
+        if (axis != 0)
+        {
+            ChangeSliderValue(axis * 20);
+        } 
+    }
+
+    public void ChangeSliderValue(float axis)
+    {
+        if (slider.minValue <= slider.value + axis)
+        {
+            slider.value += axis;
+        }
+        else
+        {
+            slider.value = slider.minValue;
+        }
+
+        if (slider.maxValue >= slider.value + axis)
+        {
+            slider.value += axis;
+        }
+        else
+        {
+            slider.value = slider.maxValue;
+        }
+
+        ChangeCameraSize();
+    }
 }
