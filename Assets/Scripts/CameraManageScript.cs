07ec0bd1118a8299b4004dde4293e6e88e2c25b7
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-02 15:05:23 +0500

    Initialize CameraManageScript.cs for controlling basic functions of the main camera

---
+++ Assets/Scripts/CameraManageScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraManageScript : MonoBehaviour
+{
+	public void ChangeCameraSize()
+	{
+	    Camera.main.orthographicSize = 100 - gameObject.GetComponent<Slider>().value;
+	}
+}
