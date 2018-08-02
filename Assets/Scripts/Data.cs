513ddcaa4b3e4f1d56754811dd77213df3056ab6
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-03 01:31:12 +0500

    Чистка кода

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,20 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Assets.Scripts;
+﻿using Assets.Scripts;
+
 
 /// <summary>
 /// Главный класс из которого берутся данные
 /// </summary>
-public class Data
+public static class Data
 {
-    public void StartBugSelection()
+
+
+    public static void StartBugSelection()
     {
         BugsCollection.StartExecution();
     }
 
-    public BugCollection BugsCollection;
-
-    public Map Map { get; set; }
-
+    public static BugCollection BugsCollection;
 }
