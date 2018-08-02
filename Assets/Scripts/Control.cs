0727705b435f38f122f2abd376c25b65f4029135
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-03 01:32:06 +0500

    Добавлен класс для управления основной логикой проекта

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    private void Start()
+    {
+        RenderingScript.CreateCells();
+        Map.CreateMap();
+        RenderingScript.InitializeObjects();
+    }
+}
