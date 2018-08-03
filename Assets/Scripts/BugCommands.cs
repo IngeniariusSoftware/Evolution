6820cb8e42f79dfb5301583c6c9c1a4ba578fbf4
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-03 20:00:07 +0500

    Класс для хранения команд жука

---
+++ Assets/Scripts/BugCommands.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BugCommands
+{
+    
+
+    private static bool Move(Bug bug)
+    {
+        int destinationX = bug.Gene.genome[bug.NextGenePosition(1)] / 8
+        int destinationY = 
+    }
+
+    private static bool CheckCell(Bug bug)
+    {
+
+    }
+
+    private static bool Take(Bug bug)
+    {
+
+    }
+
+    private static bool Rotate(Bug bug)
+    {
+
+    }
+
+    private static bool Multiply(Bug bug)
+    {
+
+    }
+
+    private static bool Push(Bug bug)
+    {
+
+    }
+
+    private static bool CheckHealth(Bug bug)
+    {
+
+    }
+
+    private static bool Attack(Bug bug)
+    {
+
+    }
+
+    private static bool Share(Bug bug)
+    {
+
+    }
+
+    private static bool GenomJump(Bug bug)
+    {
+
+    }
+}
