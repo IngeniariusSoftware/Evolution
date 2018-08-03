4189a94b1127d4863419930909041732fcaab4cb
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-03 19:59:36 +0500

    Класс для получения координатного сдвига

---
+++ Assets/Scripts/Coordinates.cs
@@ -0,0 +1,20 @@
+﻿public class Coordinates
+{
+    public int Y { get; set; }
+
+    public int X { get; set; }
+
+    public static Coordinates[] CoordinateShift = new Coordinates[]
+                                                      {
+                                                          new Coordinates(-1, 0), new Coordinates(-1, 1),
+                                                          new Coordinates(0, 1), new Coordinates(1, 1),
+                                                          new Coordinates(1, 0), new Coordinates(1, -1),
+                                                          new Coordinates(0, -1), new Coordinates(-1, -1)
+                                                      };
+
+    public Coordinates(int y, int x)
+    {
+        Y = y;
+        X = x;
+    }
+}
