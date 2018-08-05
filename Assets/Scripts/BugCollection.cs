911e0f6701484b59645171f149331b20a60c2e2a
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-05 23:11:01 +0500

    Реализован класс BugCollection. Добавлен конструктор. Добавлена функция создания новых жуков. Добавлена логика для функции StartExecution

--- Assets/Scripts/BugCollection.cs
+++ Assets/Scripts/BugCollection.cs
@@ -1,56 +1,107 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-namespace Assets.Scripts
+/// <summary>
+/// Класс, в котором хранится коллекция жуков
+/// </summary>
+public class BugCollection
 {
-    /// <summary>
-    /// Класс, в котором хранится коллекция жуков
-    /// </summary>
-    public class BugCollection
+    public int Count;
+
+    public List<Bug> Bugs { get; set; }
+
+    public BugCollection()
     {
-        public BugCollection()
-        {
-            Bugs = new List<Bug>();
-        }
+        Bugs = new List<Bug>();
+        Count = 0;
+    }
 
-        /// <summary>
-        /// Конструктор выполняющий полное копирование прилетевших значений
-        /// </summary>
-        /// <param name="inBugs"></param>
-        public BugCollection(IList<Bug> inBugs)
+    public BugCollection(int countBug)
+    {
+        Bugs = new List<Bug>();
+        for (int i = 0; i < countBug; i++)
         {
-            Bugs = inBugs.Select(x=>x).ToList();
+            Bugs.Add(new Bug());
+            Count++;
         }
-        public IList<Bug> Bugs { get; set; }
+    }
 
-        /// <summary>
-        /// Создает новое поколение жуков
-        /// </summary>
-        public void CreateNewGeneration()
+    /// <summary>
+    /// Начать действие над коллекцией жуков
+    /// </summary>
+    public void StartExecution()
+    {
+        List<Bug> deadBugs = new List<Bug>();
+        foreach (var bug in Bugs)
         {
-           
-        }
 
-        /// <summary>
-        /// Сортировка жуков по значению здоровья для дальнейшего отбора
-        /// </summary>
-        private void SortBugs()
-        {
-            Bugs = Bugs.OrderBy(x => x.Health).ToList();
-            //Bugs= Array.Sort(Bugs.ToArray());
-        }
-
-        /// <summary>
-        /// Начать действие над коллекцией жуков
-        /// </summary>
-        public void StartExecution()
-        {
-            foreach (var bug in Bugs)
+            if (bug.Health == 0)
+            {
+                if (Count > 10)
+                {
+                    deadBugs.Add(bug);
+                    Count--;
+                }
+            }
+            else
             {
                 bug.StartAction();
             }
         }
+
+        foreach (var bug in deadBugs)
+        {
+            Map.WorldMap[bug.Coordinate.Y, bug.Coordinate.X].CellType = CellEnum.TypeOfCell.Empty;
+            Bugs.Remove(bug);
+        }
     }
+
+    public void NewGeneration()
+    {
+        
+        List<Bug> bugs = new List<Bug>();
+        for (int i = 0; i < Count * 10; i++)
+        {
+            bugs.Add(new Bug(new Genome(Bugs[i / 10].Gene.GenomeMutate(Data.Rnd.Next(0,2)))));
+        }
+
+        foreach (var bug in Bugs)
+        {
+            Map.WorldMap[bug.Coordinate.Y, bug.Coordinate.X].CellType = CellEnum.TypeOfCell.Empty;
+        }
+
+        Count = bugs.Count;
+        Bugs = bugs;
+    }
+
+    ///// <summary>
+    ///// Сортировка жуков по значению здоровья для дальнейшего отбора
+    ///// </summary>
+    //private void SortBugs()
+    //{
+    //    Bugs = Bugs.OrderBy(x => x.Health).ToList();
+    //    //Bugs= Array.Sort(Bugs.ToArray());
+    //}
+
+
+    ///// <summary>
+    ///// Конструктор выполняющий полное копирование прилетевших значений
+    ///// </summary>
+    ///// <param name="inBugs"></param>
+    //public BugCollection(IList<Bug> inBugs)
+    //{
+    //    Bugs = inBugs.Select(x => x).ToList();
+    //}
+
+    ///// <summary>
+    ///// Создает новое поколение жуков
+    ///// </summary>
+    //public void CreateNewGeneration()
+    //{
+    //}
+
+
+
+
 }
+
