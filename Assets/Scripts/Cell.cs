e0714984fac34710e79858d9db556b123631c70b
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-05 17:40:21 +0500

    Добавил логику в свойства типа клетки для контроля за количеством яда и еды на карте

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -5,15 +5,57 @@ using UnityEngine;
 public class Cell
 {
     public Coordinates Coordinate { get; set; }
+    
 
     public Bug LinkedBug { get; set; }
 
-    public CellEnum.TypeOfCell CellType; // Вот тут запилить события, когда клетку изменяешь, что-нибудь навешать
+    private CellEnum.TypeOfCell _cellType = CellEnum.TypeOfCell.Empty;
+
+    public CellEnum.TypeOfCell CellType
+    {
+        get
+        {
+            return _cellType;
+        }
+
+        set
+        {
+            switch (_cellType)
+            {
+                case CellEnum.TypeOfCell.Food:
+                    {
+                        Data.CurrentCoutFood--;
+                        break;
+                    }
+                case CellEnum.TypeOfCell.Poison:
+                    {
+                        Data.CurrentCoutPoison--;
+                        break;
+                    }
+            }
+
+            switch (value)
+            {
+                case CellEnum.TypeOfCell.Food:
+                    {
+                        Data.CurrentCoutFood++;
+                        break;
+                    }
+                case CellEnum.TypeOfCell.Poison:
+                    {
+                        Data.CurrentCoutPoison++;
+                        break;
+                    }
+            }
+
+            _cellType = value;
+        }
+    }
+
 
     public Cell(Coordinates coordinate, CellEnum.TypeOfCell cellType, Bug bug = null)
     {
-        Coordinate.Y = coordinate.Y;
-        Coordinate.X = coordinate.X;
+        Coordinate = coordinate;
         CellType = cellType;
         LinkedBug = bug;
         //if (cellType == CellEnum.TypeOfCell.Bug && bug == null)
