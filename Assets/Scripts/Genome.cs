f7af7b40f382e13592005b747ec1d6d08509ec99
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-05 23:06:19 +0500

    Реализован класс геном. Добавлены конструкторы. Функция мутирования генома

--- Assets/Scripts/Genome.cs
+++ Assets/Scripts/Genome.cs
@@ -1,33 +1,59 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-
-namespace Assets.Scripts
+﻿public class Genome
 {
-    public class Genome : IEnumerable,ICloneable
+    public int[] genome = new int[Data.LengthGenome];
+
+    public int[] GenomeMutate(int countMutation)
     {
-        public List<int> genome { get; set; }
+        int[] mutateGenome = (int[])genome.Clone();
 
-        public IEnumerator GetEnumerator()
+        for (int i = 0; i < countMutation; i++)
         {
-            foreach (var gene in genome) yield return gene;
+            mutateGenome[Data.Rnd.Next(0, mutateGenome.Length)] = Data.Rnd.Next(0, Data.LengthGenome);
         }
 
-        /// <summary>
-        /// Реализация IClonable для глубокого копирования
-        /// </summary>
-        /// <returns></returns>
-        public object Clone()
+        return mutateGenome;
+    }
+
+    public Genome()
+    {
+        for (int i = 0; i < genome.Length; i++)
         {
-            return new List<int>(genome);
-        }
-        /// <summary>
-        /// Клонирование генома в виде List<int>, чтобы не выполнялось клонирование по ссылке 
-        /// </summary>
-        /// <returns></returns>
-        public List<int> Clone(bool flag)
-        {
-            return new List<int>(genome); 
+            genome[i] = Data.Rnd.Next(0, Data.LengthGenome);
         }
     }
+
+    public Genome(int[] newGenome)
+    {
+        genome = newGenome;
+    }
 }
+
+//public class Genome : IEnumerable,ICloneable
+    //{
+    //    public int[] genome;
+
+
+    //    public List<int> genome { get; set; }
+
+    //    public IEnumerator GetEnumerator()
+    //    {
+    //        foreach (var gene in genome) yield return gene;
+    //    }
+
+    //    /// <summary>
+    //    /// Реализация IClonable для глубокого копирования
+    //    /// </summary>
+    //    /// <returns></returns>
+    //    public object Clone()
+    //    {
+    //        return new List<int>(genome);
+    //    }
+    //    /// <summary>
+    //    /// Клонирование генома в виде List<int>, чтобы не выполнялось клонирование по ссылке 
+    //    /// </summary>
+    //    /// <returns></returns>
+    //    public List<int> Clone(bool flag)
+    //    {
+    //        return new List<int>(genome); 
+    //    }
+    //}
