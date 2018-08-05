97c3c782d13e9c3c449f181e16028660f1a8507e
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-05 17:45:56 +0500

    1) Добавил генерацию стен
    2) Добавил функцию поддержания установленного уровня еды и яда на карте

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,32 +4,62 @@ using Random = UnityEngine.Random;
 
 public class Map
 {
-    public static Coordinates Size = new Coordinates(100, 100);
 
-    public static Cell[,] WorldMap =  new Cell[Size.Y, Size.X];
+    public static Cell[,] WorldMap =  new Cell[Data.MapSize.Y, Data.MapSize.X];
 
     public IEnumerator GetEnumerator()
     {
         if (WorldMap != null)
         {
-            for (int x = 0; x < Size.X; x++)
+            for (int x = 0; x < Data.MapSize.X; x++)
             {
-                for (int y = 0; y < Size.Y; y++)
+                for (int y = 0; y < Data.MapSize.Y; y++)
                 {
                     yield return WorldMap[y, x];
                 }
             }
         }
     }
-    
+
+    /// <summary>
+    /// Поддержание уровня еды, яда на карте
+    /// </summary>
+    public static void RefreshMap()
+    {
+        while (Data.CurrentCoutFood < Data.MaxCountFood)
+        {
+            Coordinates randomPosition = Coordinates.RandomCoordinates(Data.MapSize.Y, Data.MapSize.X);
+            if (Map.WorldMap[randomPosition.Y, randomPosition.X].CellType == CellEnum.TypeOfCell.Empty)
+            {
+                Map.WorldMap[randomPosition.Y, randomPosition.X].CellType = CellEnum.TypeOfCell.Food;
+            }
+        }
+
+        while (Data.CurrentCoutPoison < Data.MaxCountPoison)
+        {
+            Coordinates randomPosition = Coordinates.RandomCoordinates(Data.MapSize.Y, Data.MapSize.X);
+            if (Map.WorldMap[randomPosition.Y, randomPosition.X].CellType == CellEnum.TypeOfCell.Empty)
+            {
+                Map.WorldMap[randomPosition.Y, randomPosition.X].CellType = CellEnum.TypeOfCell.Poison;
+            }
+        }
+    }
+
 
     public static void CreateMap()
     {
-        for (int x = 0; x < Size.X; x++)
+        for (int x = 0; x < Data.MapSize.X; x++)
         {
-            for (int y = 0; y < Size.Y; y++)
+            for (int y = 0; y < Data.MapSize.Y; y++)
             {
-                WorldMap[y, x] = new Cell(new Coordinates(y, x), CellEnum.GetCellType(Random.Range(0,5)));
+                if (x == 0 || x == Data.MapSize.X - 1 || y == 0 || y == Data.MapSize.Y - 1)
+                {
+                    WorldMap[y, x] = new Cell(new Coordinates(y, x), CellEnum.TypeOfCell.Wall);
+                }
+                else
+                {
+                    WorldMap[y, x] = new Cell(new Coordinates(y, x), CellEnum.GetCellType(Random.Range(0, 5)));
+                }
             }
         }
     }
