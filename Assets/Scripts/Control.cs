3fe33f3ba3cdb64baab186cec8e00cc4276b0faa
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-05 23:08:34 +0500

    Добавлена основная логики в класс управления всеми частями проекта

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -1,16 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-using Assets.Scripts;
-
+﻿using System.Threading;
 using UnityEngine;
 
 public class Control : MonoBehaviour
 {
+    public static BugCollection bugs = new BugCollection();
+
     void Start()
     {
-       // RenderingScript.CreateCells();
-        Map.CreateMap();
+        // RenderingScript.CreateCells();
+        
         RenderingScript.InitializeObjects();
+        Map.CreateMap();
+        bugs = new BugCollection(Data.BugCount);
+    }
+
+    void Update()
+    {
+        bugs.StartExecution();
+        Map.RefreshMap();
+        if (bugs.Count == 10)
+        {
+            bugs.NewGeneration();
+        }
+
+        Thread.Sleep(50);
     }
 }
