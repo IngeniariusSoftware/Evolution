ffcb14072d021abcc53a2834adc258de73fdbb79
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-05 23:06:59 +0500

    Добавлена глобальая переменная для генерации случайных чисел. Добавлено поле количества жуков. Добавлены поля, отвечающие за количестов стен.

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Scripts;
-
+﻿using System;
 
 /// <summary>
 /// Главный класс из которого берутся данные
@@ -7,6 +6,16 @@
 public static class Data
 {
     #region Переменные
+
+    /// <summary>
+    ///     Переменная для генерации случайный значений
+    /// </summary>
+    public static readonly Random Rnd = new Random();
+
+    /// <summary>
+    ///     Начальное количество жуков
+    /// </summary>
+    public static readonly int BugCount = 100;
 
     /// <summary>
     ///     Количество команд в геноме
@@ -31,37 +40,52 @@ public static class Data
     /// <summary>
     ///     Количество клеток по x и y   
     /// </summary>
-    public static readonly Coordinates MapSize = new Coordinates(50, 50);
+    public static readonly Coordinates MapSize = new Coordinates(35, 60);
 
     /// <summary>
     ///     Процент еды на карте
     /// </summary>
-    public static readonly float PercentFood = 0.05f;
+    public static readonly float PercentFood = 0.1f;
 
     /// <summary>
     ///      Процент яда на карте
     /// </summary>
-    public static readonly float PercentPoison = 0.05f;
+    public static readonly float PercentPoison = 0.1f;
+
+    /// <summary>
+    ///      Процент яда на карте
+    /// </summary>
+    public static readonly float PercentWall = 0.05f;
 
     /// <summary>
     ///     Текущее количество еды на карте
     /// </summary>
-    public static int CurrentCoutFood = 0;
+    public static int CurrentCountFood = 0;
 
     /// <summary>
     ///    Текущее количество яда на карте
     /// </summary>
-    public static int CurrentCoutPoison = 0;
+    public static int CurrentCountPoison = 0;
+
+    /// <summary>
+    ///    Текущее количество яда на карте
+    /// </summary>
+    public static int CurrentCountWall = 0;
 
     /// <summary>
     ///     Максимально возможное количество еды на карте
     /// </summary>
     public static readonly int MaxCountFood = (int)(MapSize.X * MapSize.Y * PercentFood);
-    
+
     /// <summary>
     ///     Максимально возможное количество яда на карте
     /// </summary>
     public static readonly int MaxCountPoison = (int)(MapSize.X * MapSize.Y * PercentPoison);
+
+    /// <summary>
+    ///     Максимально возможное количество яда на карте
+    /// </summary>
+    public static readonly int MaxCountWall = (int)(MapSize.X * MapSize.Y * PercentWall);
 
     /// <summary>
     ///     Размер клетки по абсциссе для корректного отображения спрайта
