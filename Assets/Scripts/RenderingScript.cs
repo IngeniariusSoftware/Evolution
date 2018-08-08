fb35c26840f69d6e9d80d947d3b75619c508084b
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-08 12:54:46 +0500

    Добавил метод для отрисовки всех событий на поле. Метод громоздкий и использует множество временных полей, требует оптимизации, однако, все равно работает неплохо

--- Assets/Scripts/RenderingScript.cs
+++ Assets/Scripts/RenderingScript.cs
@@ -1,22 +1,42 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 using UnityEngine;
 
 public class RenderingScript : MonoBehaviour
 {
-    // Не совсем красивое решение, так как можно считывать размеры картинки
-    // и формировать размеры клетки, но у нас единый стандарт, так что без разницы
-
+    public static Color Capacity = new Color(1, 1, 1, 0);
 
     /// <summary>
-    ///     Объект
+    ///     Лист всех клеток
+    /// </summary>
+    public static List<Cell> RendredCells = new List<Cell>();
+
+    /// <summary>
+    ///     Лист всех клеток
+    /// </summary>
+    public static List<Bug> RendredCellsBug = new List<Bug>();
+
+    /// <summary>
+    ///     Лист всех изображений сущностей
+    /// </summary>
+    public static List<GameObject> RenderingBugs = new List<GameObject>();
+
+    /// <summary>
+    ///     Лист всех изображений сущностей
+    /// </summary>
+    public static List<GameObject> RenderingObjects = new List<GameObject>();
+
+    /// <summary>
+    ///     Лист всех изображений сущностей
     /// </summary>
     public static List<Sprite> Sprites = new List<Sprite>();
 
     /// <summary>
-    ///     Объект
+    ///     Объект карты, на который вешаются изображения
     /// </summary>
-    public static Transform Object;
+    public static GameObject Object;
 
     /// <summary>
     ///     Массив всех объектов Unity на карте
@@ -25,30 +45,13 @@ public class RenderingScript : MonoBehaviour
 
     void Awake()
     {
-        Object = Resources.Load<GameObject>("Empty").transform;
+        Object = Resources.Load<GameObject>("Empty");
         for (int i = 0; i < 7; i++)
         {
             Sprites.Add(
                 Resources.Load<Sprite>("Sprites/" + CellEnum.GetCellType(i).ToString().Replace("TypeOfCell.", "")));
         }
     }
-
-    /// <summary>
-    ///     Сгенировать карту из пустых клеток
-    /// </summary>
-    //public static void CreateCells()
-    //{
-    //    /* Генерируем пустые клетки на экране
-    //    в центре экрана точка отсчета, поэтому
-    //    начинаем генерировать со смещением */
-    //    for (int y = -Data.MapSize.Y / 2; y < Math.Ceiling(Data.MapSize.Y / 2.0); y++)
-    //    {
-    //        for (int x = -Data.MapSize.X / 2; x < Math.Ceiling(Data.MapSize.X / 2.0); x++)
-    //        {
-    //            Instantiate(Objects[(int)CellEnum.TypeOfCell.Empty], new Vector3(x * Data.CellSizeX, y * Data.CellSizeY), new Quaternion(0, 0, 0, 0));
-    //        }
-    //    }
-    //}
 
     /// <summary>
     ///     Сгенировать все объекты на карте
@@ -60,7 +63,7 @@ public class RenderingScript : MonoBehaviour
             for (int y = 0; y < Data.MapSize.Y; y++)
             {
                 MapObjects[y, x] = Instantiate(
-                    Object,
+                    Object.transform,
                     new Vector3((x - Data.MapSize.X / 2) * Data.CellSizeX, (y - Data.MapSize.Y / 2) * Data.CellSizeY),
                     new Quaternion(0, 0, 0, 0));
 
@@ -71,16 +74,128 @@ public class RenderingScript : MonoBehaviour
     /// <summary>
     ///     Обновить положения объекта на карте
     /// </summary>
-    public static void UpdateObjects(Cell cell)
+    public static void UpdateObjects()
     {
-        MapObjects[cell.Coordinate.Y, cell.Coordinate.X].transform.position = new Vector3(
-            (cell.Coordinate.Y - Data.MapSize.Y / 2) * Data.CellSizeY,
-            (cell.Coordinate.X - Data.MapSize.X / 2) * Data.CellSizeX);
+        Capacity.a = (float)Data.CurrentStepsRendering / Data.MaxStepsRendering;
+        if (Data.CurrentStepsRendering == 0)
+        {
+            foreach (Cell rendredCell in RendredCells)
+            {
+                if (rendredCell.LinkedBug != null)
+                {
+                    if (rendredCell.LinkedBug.LastPosition != null)
+                    {
+                        RenderingBugs.Add(
+                            Instantiate(
+                                Object,
+                                new Vector3(
+                                    (rendredCell.LinkedBug.LastPosition.X - Data.MapSize.X / 2) * Data.CellSizeX,
+                                    (rendredCell.LinkedBug.LastPosition.Y - Data.MapSize.Y / 2) * Data.CellSizeY,
+                                    -3),
+                                new Quaternion(0, 0, 0, 0)));
+                        RenderingBugs.Last().GetComponent<SpriteRenderer>().sprite =
+                            Sprites[(int)CellEnum.TypeOfCell.Bug];
+                      MapObjects[rendredCell.LinkedBug.LastPosition.Y, rendredCell.LinkedBug.LastPosition.X].GetComponent<SpriteRenderer>().sprite = Sprites[(int)CellEnum.TypeOfCell.Empty];
+                        RendredCellsBug.Add(rendredCell.LinkedBug);
+                    }
+                    else
+                    {
+                        RenderingObjects.Add(
+                            Instantiate(
+                                Object,
+                                new Vector3(
+                                    (rendredCell.Coordinate.X - Data.MapSize.X / 2) * Data.CellSizeX,
+                                    (rendredCell.Coordinate.Y - Data.MapSize.Y / 2) * Data.CellSizeY,
+                                    -3),
+                                new Quaternion(0, 0, 0, 0)));
+                        RenderingObjects.Last().GetComponent<SpriteRenderer>().sprite =
+                            Sprites[(int)CellEnum.TypeOfCell.Bug];
+                        Color colorObject = RenderingObjects.Last().GetComponent<SpriteRenderer>().color = Capacity;
+                    }
+
+                }
+                else
+                {
+                    RenderingObjects.Add(
+                        Instantiate(
+                            Object,
+                            new Vector3(
+                                (rendredCell.Coordinate.X - Data.MapSize.X / 2) * Data.CellSizeX,
+                                (rendredCell.Coordinate.Y - Data.MapSize.Y / 2) * Data.CellSizeY,
+                                -2),
+                            new Quaternion(0, 0, 0, 0)));
+                    RenderingObjects.Last().GetComponent<SpriteRenderer>().sprite = Sprites[(int)rendredCell.CellType];
+                    RenderingObjects.Last().GetComponent<SpriteRenderer>().color = Capacity;
+                }
+            }
+
+            Data.CurrentStepsRendering++;
+        }
+        else
+        {
+            if (Data.CurrentStepsRendering < Data.MaxStepsRendering)
+            {
+                for (int i = 0; i < RenderingBugs.Count; i++)
+                {
+                    Vector2 currentPosition = new Vector2(
+                        RendredCellsBug[i].CurrentPosition.X,
+                        RendredCellsBug[i].CurrentPosition.Y);
+                    Vector2 lastPosition = new Vector2(
+                        RendredCellsBug[i].LastPosition.X,
+                        RendredCellsBug[i].LastPosition.Y);
+                    RenderingBugs[i].transform.position +=
+                        (Vector3)((currentPosition - lastPosition) * Data.CellSizeX / Data.MaxStepsRendering); // Размер клетки только по х, опасно
+                }
+
+                foreach (GameObject renderingObject in RenderingObjects)
+                {
+                    renderingObject.GetComponent<SpriteRenderer>().color = Capacity;
+                }
+            }
+            else
+            {
+
+                foreach (Cell rendredCell in RendredCells)
+                {
+                    MapObjects[rendredCell.Coordinate.Y, rendredCell.Coordinate.X].GetComponent<SpriteRenderer>().sprite
+                        = Sprites[(int)rendredCell.CellType];
+                }
+
+
+                foreach (var renderingBug in RenderingBugs)
+                {
+                    Destroy(renderingBug);
+                }
+
+                foreach (var renderingObject in RenderingObjects)
+                {
+                    Destroy(renderingObject);
+                }
+
+                RenderingObjects.Clear();
+                RendredCells.Clear();
+                RenderingBugs.Clear();
+                RendredCellsBug.Clear();
+            }
+
+            Data.CurrentStepsRendering++;
+        }
     }
 
     public static void UpdateTypeCell(Cell cell)
     {
-        MapObjects[cell.Coordinate.Y, cell.Coordinate.X].GetComponent<SpriteRenderer>().sprite =
-            Sprites[(int)cell.CellType];
+        if (!RendredCells.Contains(cell)) //TODO Разобраться с этим
+        {
+            RendredCells.Add(cell);
+        }
+        else
+        {
+            RendredCells[RendredCells.IndexOf(cell)] = cell;
+        }
+
+        //Color g =  MapObjects[cell.Coordinate.Y, cell.Coordinate.X].GetComponent<SpriteRenderer>().color;
+        // g.a = 0;
+        // MapObjects[cell.Coordinate.Y, cell.Coordinate.X].GetComponent<SpriteRenderer>().color = g;
+        // // MapObjects[cell.CurrentPosition.Y, cell.CurrentPosition.X].
     }
 }
