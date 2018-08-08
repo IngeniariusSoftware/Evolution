cfb716734123c00e574670655a34b64a6adfe96f
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-08 20:37:29 +0500

    Создал скрипт для изменения времени

---
+++ Assets/Scripts/TimeManageScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Slider = UnityEngine.UI.Slider;
+
+public class TimeManageScript : MonoBehaviour
+{
+    public Slider slider;
+
+    public static int TimeSpeed;
+
+    void Awake()
+    {
+        TimeSpeed = (int)slider.value;
+    }
+
+    public void ChangeTimeSped()
+    {
+        TimeSpeed = (int)slider.value;
+    }
+}
