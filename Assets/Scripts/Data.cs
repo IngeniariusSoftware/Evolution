1b45fae5c804ff921ef594ee70a8027f19443105
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-08 20:36:28 +0500

    Некоторые константы одного типа объединил в массивы

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -6,12 +6,7 @@
 public static class Data
 {
     #region Переменные
-
-    /// <summary>
-    ///     Максимальный шаг отрисовки (Влияет на плавность отрисовки хода)
-    /// </summary>
-    public static readonly int MaxStepsRendering = 24;
-
+    
     /// <summary>
     ///     Максимальное количество команд, которое может выполнить жук за один ход
     /// </summary>
@@ -63,44 +58,33 @@ public static class Data
     public static readonly Coordinates MapSize = new Coordinates(70, 120);
 
     /// <summary>
-    ///     Процент обычных ягод на карте
+    ///     Процент от общего числа клеток различных объектов на карте
     /// </summary>
-    public static readonly float PercentBerry = 0.06f;
+    public static readonly float[] PercentObjects = {0, 0.06f, 0.06f, 0.08f, 0.03f, 0, 0};
 
     /// <summary>
-    ///      Процент яда на карте
+    ///     Общее количетсво клеток на карте
     /// </summary>
-    public static readonly float PercentPoison = 0.06f;
+    private static readonly int AllCellCount = MapSize.X * MapSize.Y;
 
     /// <summary>
-    ///      Процент минералов на карте
+    ///     Максимально возможное количество различных объектов на карте
     /// </summary>
-    public static readonly float PercentMineral = 0.04f;
+    public static readonly int[] MaxCountObjects =
+        {
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Empty]),
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Berry]),
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Poison]),
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Wall]),
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Mineral]),
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.MineralBerry]),
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Bug])
+        };
 
     /// <summary>
-    ///      Процент стен на карте
+    ///     Текущее количество различных объектов на карте
     /// </summary>
-    public static readonly float PercentWall = 0.08f;
-
-    /// <summary>
-    ///     Максимально возможное количество минералов на карте
-    /// </summary>
-    public static readonly int MaxCountMineral = (int)(MapSize.X * MapSize.Y * PercentMineral);
-
-    /// <summary>
-    ///     Максимально возможное количество обычных ягод на карте
-    /// </summary>
-    public static readonly int MaxCountBerry = (int)(MapSize.X * MapSize.Y * PercentBerry);
-
-    /// <summary>
-    ///     Максимально возможное количество яда на карте
-    /// </summary>
-    public static readonly int MaxCountPoison = (int)(MapSize.X * MapSize.Y * PercentPoison);
-
-    /// <summary>
-    ///     Максимально возможное количество стен на карте
-    /// </summary>
-    public static readonly int MaxCountWall = (int)(MapSize.X * MapSize.Y * PercentWall);
+    public static readonly int[] CurrentCountObjects = { 0, 0, 0, 0, 0, 0, 0 };
 
     /// <summary>
     ///     Размер клетки по абсциссе для корректного отображения спрайта
@@ -113,29 +97,14 @@ public static class Data
     public static readonly float CellSizeY = 2.56f;
 
     /// <summary>
-    ///     Текущее количество минералов на карте
-    /// </summary>
-    public static int CurrentCountMineral = 0;
-
-    /// <summary>
-    ///     Текущее количество обычных ягод на карте
-    /// </summary>
-    public static int CurrentCountBerry = 0;
-
-    /// <summary>
-    ///    Текущее количество яда на карте
-    /// </summary>
-    public static int CurrentCountPoison = 0;
-
-    /// <summary>
-    ///    Текущее количество стен на карте
-    /// </summary>
-    public static int CurrentCountWall = 0;
-
-    /// <summary>
     ///     Текущий шаг отрисовки
     /// </summary>
     public static int CurrentStepsRendering = 0;
+
+    /// <summary>
+    ///     Максимальный шаг отрисовки (Влияет на плавность отрисовки хода)
+    /// </summary>
+    public static int MaxStepsRendering = 24;
 
     #endregion
 }
