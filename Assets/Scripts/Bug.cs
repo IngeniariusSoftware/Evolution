0af0e51b952a07aea96db491fd2e8042249e7b08
Кирилл Грищук <kirya522@gmail.com>
2018-08-10 15:51:04 +0500

    подправил класс жука, вынес непонятные куски в методы отдельные, добавил комментарии, добавил значение по умолчанию для смещения

--- Assets/Scripts/Bug.cs
+++ Assets/Scripts/Bug.cs
@@ -1,103 +1,20 @@
-﻿using System;
-using System.Diagnostics;
-using UnityEngine;
-
-using Random = System.Random;
-
-public class Bug
+﻿public class Bug
 {
-    public Coordinates LastPosition { get; set; }
-
-    public Coordinates CurrentPosition { get; set; }
-
-    public int LifeTime { get; set; }
-
-    private int _health;
-
-    public int Health
-    {
-        get
-        {
-            return _health;
-        }
-
-        set
-        {
-            if (value > -1 && value <= Data.MaxBugHealth)
-            {
-                _health = value;
-            }
-            else
-            {
-                _health = 0;
-            }
-        }
-    }
-
-    public Genome Gene { get; set; }
-
-    private int _currentGenePosition;
-
-    public int CurrentGenePosition
-    {
-        get
-        {
-            return _currentGenePosition;
-        }
-
-        set
-        {
-            _currentGenePosition = value % Data.LengthGenome;
-        }
-    }
-
-    public int Direction { get; set; }
-
-    public void StartAction()
-    {
-        LastPosition = CurrentPosition;   
-        int countSteps = 0;
-        bool isEnd = false;
-        Health--;
-        LifeTime++;
-        while (countSteps < Data.MaxStepsBug && !isEnd)
-        {
-            isEnd = DoCommand(bug: this);
-            countSteps++;
-        }
-
-        if (countSteps == Data.MaxStepsBug && !isEnd)
-        {
-            Health = 0;
-        }
-    }
-
-    public int NextGenePosition(int shift)
-    {
-        return (CurrentGenePosition + shift) % Data.LengthGenome;
-    }
-
+    // Зачем здесь логика на спавн???
     public Bug(Genome genome = null, Coordinates currentPosition = null)
     {
         if (currentPosition == null)
-        {
             do
             {
                 currentPosition = Coordinates.RandomCoordinates(Data.MapSize.Y, Data.MapSize.X);
-            }
-            while (Map.WorldMap[currentPosition.Y, currentPosition.X].CellType != CellEnum.TypeOfCell.Empty);
-        }
+            } while (Map.WorldMap[currentPosition.Y, currentPosition.X].CellType != CellEnum.TypeOfCell.Empty);
 
         CurrentPosition = currentPosition;
         Map.WorldMap[currentPosition.Y, currentPosition.X].CellType = CellEnum.TypeOfCell.Bug;
         if (genome == null)
-        {
             Gene = new Genome();
-        }
         else
-        {
             Gene = genome;
-        }
 
         Health = Data.StartBugHealth;
         LifeTime = 0;
@@ -105,78 +22,156 @@ public class Bug
         Direction = Data.Rnd.Next(0, 8);
         Map.WorldMap[currentPosition.Y, currentPosition.X].LinkedBug = this;
     }
-    
+
+    #region BugAttributes
+
+    public int Direction { get; set; }
+
+    public int LifeTime { get; set; }
+
+    private int _health;
+
+    public int Health
+    {
+        get { return _health; }
+
+        set
+        {
+            if (value > -1 && value <= Data.MaxBugHealth)
+                _health = value;
+            else
+                _health = 0;
+        }
+    }
+
+    public Genome Gene { get; set; }
+
+    #endregion
+
+    #region Genome
+
+    private int _currentGenePosition;
+
+    public int CurrentGenePosition
+    {
+        get { return _currentGenePosition; }
+
+        set { _currentGenePosition = value % Data.LengthGenome; }
+    }
+
+    private static bool GenomJump(Bug bug)
+    {
+        bug.CurrentGenePosition = bug.NextGenePosition(bug.Gene.genome[bug.CurrentGenePosition]);
+        return false;
+    }
+
+    public int NextGenePosition(int shift = 1)
+    {
+        return (CurrentGenePosition + shift) % Data.LengthGenome;
+    }
+
+    #endregion
+
+    #region BugPosition
+
+    public Coordinates LastPosition { get; set; }
+
+    public Coordinates CurrentPosition { get; set; }
+
     private static Cell DestinationCell;
+
+    #endregion
+
+    public void StartAction()
+    {
+        LastPosition = CurrentPosition;
+        var countSteps = 0;
+        var isEnd = false;
+        Health--;
+        LifeTime++;
+        while (countSteps < Data.MaxStepsBug && !isEnd)
+        {
+            isEnd = DoCommand(this);
+            countSteps++;
+        }
+
+        //Умирает если зациклился 
+        if (countSteps == Data.MaxStepsBug && !isEnd) Health = 0;
+    }
+
+    public static bool DoCommand(Bug bug)
+    {
+        var destination =
+            Coordinates.CoordinateShift[CalculateShift(bug)] + bug.CurrentPosition;
+        DestinationCell = Map.WorldMap[destination.Y, destination.X];
+
+        // Если комманда действие, то выполнить
+        if (MasBugCommands.Length > bug.Gene.genome[bug.CurrentGenePosition])
+            return MasBugCommands[bug.Gene.genome[bug.CurrentGenePosition]].Invoke(bug);
+        // Иначе сделать переход по геному
+        return GenomJump(bug);
+    }
 
     public delegate bool BugCommand(Bug bug);
 
     //TODO Команды  Move, Rotate, CheckCell, Take, Multiply, Push, CheckHealth, Attack, Share, Photosynthesize , IsFriend Вспомогательная команда, поэтому не вносится
-    public static BugCommand[] MasBugCommands = { Move, Rotate, CheckCell, Take, Multiply, Push, CheckHealth, Attack, Share};
+    public static BugCommand[] MasBugCommands = {Move, Rotate, CheckCell, Take, Multiply, Push, CheckHealth, Attack, Share};
 
-    public static bool DoCommand(Bug bug)
+    private static int CalculateShift(Bug bug)
     {
-        Coordinates destination =
-            Coordinates.CoordinateShift[((bug.Direction + bug.Gene.genome[bug.NextGenePosition(1)]) % 8)]
-            + bug.CurrentPosition;
-        DestinationCell = Map.WorldMap[destination.Y, destination.X];
-        if (MasBugCommands.Length > bug.Gene.genome[bug.CurrentGenePosition])
-        {
-            return MasBugCommands[bug.Gene.genome[bug.CurrentGenePosition]].Invoke(bug);
-        }
-        else
-        {
-            return GenomJump(bug);
-        }
+        var res = 0;
+        // С учетом текущего поворота
+        res = (bug.Direction + bug.Gene.genome[bug.NextGenePosition()]) % 8;
+        return res;
     }
+
+    #region BugCommands
 
     private static bool Move(Bug bug)
     {
-        bug.CurrentGenePosition += (int)DestinationCell.CellType + 1;
-        Bug neighbourBug = DestinationCell.LinkedBug;
-        if (neighbourBug != null && neighbourBug.IsFriendBug(bug))
-        {
-            bug.CurrentGenePosition++;
-        }
+        bug.CurrentGenePosition += (int) DestinationCell.CellType + 1;
+        var neighbourBug = DestinationCell.LinkedBug;
+        if (neighbourBug != null && neighbourBug.IsFriendBug(bug)) bug.CurrentGenePosition++;
 
         switch (DestinationCell.CellType)
         {
             case CellEnum.TypeOfCell.Empty:
-                {
-                    Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].LinkedBug = null;
-                    Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].CellType = CellEnum.TypeOfCell.Empty;
-                    bug.CurrentPosition = DestinationCell.Coordinate;
-                    DestinationCell.CellType = CellEnum.TypeOfCell.Bug;
-                    DestinationCell.LinkedBug = bug;
-                    break;
-                }
+            {
+                Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].LinkedBug = null;
+                Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].CellType = CellEnum.TypeOfCell.Empty;
+                bug.CurrentPosition = DestinationCell.Coordinate;
+                DestinationCell.CellType = CellEnum.TypeOfCell.Bug;
+                DestinationCell.LinkedBug = bug;
+                break;
+            }
 
             case CellEnum.TypeOfCell.Berry:
-                {
-                    Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].LinkedBug = null;
-                    Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].CellType = CellEnum.TypeOfCell.Empty;
-                    bug.CurrentPosition = DestinationCell.Coordinate;
-                    DestinationCell.CellType = CellEnum.TypeOfCell.Bug;
-                    DestinationCell.LinkedBug = bug;
-                    bug.Health += Data.BerryValue;
-                    break;
-                }
+            {
+                Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].LinkedBug = null;
+                Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].CellType = CellEnum.TypeOfCell.Empty;
+                bug.CurrentPosition = DestinationCell.Coordinate;
+                DestinationCell.CellType = CellEnum.TypeOfCell.Bug;
+                DestinationCell.LinkedBug = bug;
+                bug.Health += Data.BerryValue;
+                break;
+            }
 
             case CellEnum.TypeOfCell.MineralBerry:
-                {
-                    Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].LinkedBug = null;
-                    Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].CellType = CellEnum.TypeOfCell.Empty;
-                    bug.CurrentPosition = DestinationCell.Coordinate;
-                    DestinationCell.CellType = CellEnum.TypeOfCell.Bug;
-                    DestinationCell.LinkedBug = bug;
-                    bug.Health += Data.MineralBerryValue;
-                    break;
-                }
+            {
+                Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].LinkedBug = null;
+                Map.WorldMap[bug.CurrentPosition.Y, bug.CurrentPosition.X].CellType = CellEnum.TypeOfCell.Empty;
+                bug.CurrentPosition = DestinationCell.Coordinate;
+                DestinationCell.CellType = CellEnum.TypeOfCell.Bug;
+                DestinationCell.LinkedBug = bug;
+                bug.Health += Data.MineralBerryValue;
+                break;
+            }
 
             case CellEnum.TypeOfCell.Poison:
-                {
-                    bug.Health = 0;
-                    break;
-                }
+            {
+                bug.Health = 0;
+                break;
+            }
         }
 
         return true;
@@ -184,46 +179,40 @@ public class Bug
 
     private static bool CheckCell(Bug bug)
     {
-        bug.CurrentGenePosition += (int)DestinationCell.CellType + 1;
-        Bug neighbourBug = DestinationCell.LinkedBug;
-        if (neighbourBug != null && neighbourBug.IsFriendBug(bug))
-        {
-            bug.CurrentGenePosition++;
-        }
+        bug.CurrentGenePosition += (int) DestinationCell.CellType + 1;
+        var neighbourBug = DestinationCell.LinkedBug;
+        if (neighbourBug != null && neighbourBug.IsFriendBug(bug)) bug.CurrentGenePosition++;
 
         return false;
     }
 
     private static bool Take(Bug bug)
     {
-        bug.CurrentGenePosition += (int)DestinationCell.CellType + 1;
-        Bug neighbourBug = DestinationCell.LinkedBug;
-        if (neighbourBug != null && neighbourBug.IsFriendBug(bug))
-        {
-            bug.CurrentGenePosition++;
-        }
+        bug.CurrentGenePosition += (int) DestinationCell.CellType + 1;
+        var neighbourBug = DestinationCell.LinkedBug;
+        if (neighbourBug != null && neighbourBug.IsFriendBug(bug)) bug.CurrentGenePosition++;
 
         switch (DestinationCell.CellType)
         {
             case CellEnum.TypeOfCell.Berry:
-                {
-                    DestinationCell.CellType = CellEnum.TypeOfCell.Empty;
-                    bug.Health += Data.BerryValue;
-                    break;
-                }
+            {
+                DestinationCell.CellType = CellEnum.TypeOfCell.Empty;
+                bug.Health += Data.BerryValue;
+                break;
+            }
 
             case CellEnum.TypeOfCell.MineralBerry:
-                {
-                    DestinationCell.CellType = CellEnum.TypeOfCell.Empty;
-                    bug.Health += Data.MineralBerryValue;
-                    break;
-                }
+            {
+                DestinationCell.CellType = CellEnum.TypeOfCell.Empty;
+                bug.Health += Data.MineralBerryValue;
+                break;
+            }
 
             case CellEnum.TypeOfCell.Poison:
-                {
-                    DestinationCell.CellType = CellEnum.TypeOfCell.Berry;
-                    break;
-                }
+            {
+                DestinationCell.CellType = CellEnum.TypeOfCell.Berry;
+                break;
+            }
         }
 
         return true;
@@ -231,21 +220,14 @@ public class Bug
 
     private bool IsFriendBug(Bug bug)
     {
-        bool isDifference = false;
-        for (int i = 0; i < Gene.genome.Length; i++)
-        {
+        var isDifference = false;
+        for (var i = 0; i < Gene.genome.Length; i++)
             if (Gene.genome[i] != bug.Gene.genome[i])
             {
                 if (isDifference)
-                {
                     return false;
-                }
-                else
-                {
-                    isDifference = true;
-                }
+                isDifference = true;
             }
-        }
 
         return true;
     }
@@ -260,15 +242,15 @@ public class Bug
     private static bool Multiply(Bug bug)
     {
         bug.Health -= Data.MuptiplyCost;
-        bool isBorn = false;
-        for (int i = 0; i < 8 && !isBorn; i++)
+        var isBorn = false;
+        for (var i = 0; i < 8 && !isBorn; i++)
         {
-            Coordinates birthCoordinate = bug.CurrentPosition + Coordinates.CoordinateShift[i];
+            var birthCoordinate = bug.CurrentPosition + Coordinates.CoordinateShift[i];
             if (Map.WorldMap[birthCoordinate.Y, birthCoordinate.X].CellType == CellEnum.TypeOfCell.Empty)
             {
-                Bug childBug = new Bug(
-                    genome: new Genome(bug.Gene.GenomeMutate(Data.Rnd.Next(0, 2))),
-                    currentPosition: birthCoordinate);
+                var childBug = new Bug(
+                    new Genome(bug.Gene.GenomeMutate(Data.Rnd.Next(0, 2))),
+                    birthCoordinate);
                 ControlScript.childs.Add(childBug);
                 isBorn = true;
                 childBug.Health = bug.Health / 2;
@@ -282,32 +264,25 @@ public class Bug
 
     private static bool Push(Bug bug)
     {
-        bug.CurrentGenePosition += (int)DestinationCell.CellType + 1;
-        Bug neighbourBug = DestinationCell.LinkedBug;
-        if (neighbourBug != null && neighbourBug.IsFriendBug(bug))
-        {
-            bug.CurrentGenePosition++;
-        }
+        bug.CurrentGenePosition += (int) DestinationCell.CellType + 1;
+        var neighbourBug = DestinationCell.LinkedBug;
+        if (neighbourBug != null && neighbourBug.IsFriendBug(bug)) bug.CurrentGenePosition++;
 
         if (DestinationCell.CellType != CellEnum.TypeOfCell.Wall)
         {
             if (DestinationCell.CellType == CellEnum.TypeOfCell.Bamboo)
             {
                 if (Data.Rnd.Next(0, 3) == 0)
-                {
                     DestinationCell.CellType = CellEnum.TypeOfCell.Poison;
-                }
                 else
-                {
                     DestinationCell.CellType = CellEnum.TypeOfCell.Berry;
-                }
             }
             else
             {
-                Coordinates pushDestination =
-                    Coordinates.CoordinateShift[((bug.Direction + bug.Gene.genome[bug.NextGenePosition(1)]) % 8)]
+                var pushDestination =
+                    Coordinates.CoordinateShift[(bug.Direction + bug.Gene.genome[bug.NextGenePosition(1)]) % 8]
                     + DestinationCell.Coordinate;
-                Cell pushDestinationCell = Map.WorldMap[pushDestination.Y, pushDestination.X];
+                var pushDestinationCell = Map.WorldMap[pushDestination.Y, pushDestination.X];
                 if (pushDestinationCell.CellType == CellEnum.TypeOfCell.Empty)
                 {
                     pushDestinationCell.CellType = DestinationCell.CellType;
@@ -329,56 +304,48 @@ public class Bug
     private static bool CheckHealth(Bug bug)
     {
         if (bug.Health > bug.Gene.genome[bug.NextGenePosition(1)] * Data.MaxBugHealth / Data.LengthGenome)
-        {
             bug.CurrentGenePosition += 2;
-        }
         else
-        {
             bug.CurrentGenePosition += 3;
-        }
 
         return false;
     }
 
-    private static bool Attack(Bug bug) 
+    private static bool Attack(Bug bug)
     {
-        bug.CurrentGenePosition += (int)DestinationCell.CellType + 1;
-        Bug neighbourBug = DestinationCell.LinkedBug;
-        if (neighbourBug != null && neighbourBug.IsFriendBug(bug))
-        {
-            bug.CurrentGenePosition++;
-        }
+        bug.CurrentGenePosition += (int) DestinationCell.CellType + 1;
+        var neighbourBug = DestinationCell.LinkedBug;
+        if (neighbourBug != null && neighbourBug.IsFriendBug(bug)) bug.CurrentGenePosition++;
 
         switch (DestinationCell.CellType)
         {
             case CellEnum.TypeOfCell.Mineral:
-                {
-                    DestinationCell.CellType = CellEnum.TypeOfCell.MineralBerry;
-                    break;
-                }
+            {
+                DestinationCell.CellType = CellEnum.TypeOfCell.MineralBerry;
+                break;
+            }
 
             case CellEnum.TypeOfCell.Bug:
+            {
+                if (DestinationCell.LinkedBug != null)
                 {
-
-                    if (DestinationCell.LinkedBug != null)
-                    {
-                            DestinationCell.LinkedBug.Health -= 5;
-                            bug.Health += 5;
-                    }
-
-                    break;
+                    DestinationCell.LinkedBug.Health -= 5;
+                    bug.Health += 5;
                 }
+
+                break;
+            }
 
             case CellEnum.TypeOfCell.Wall:
-                {
-                    break;
-                }
+            {
+                break;
+            }
 
             default:
-                {
-                    DestinationCell.CellType = CellEnum.TypeOfCell.Empty;
-                    break;
-                }
+            {
+                DestinationCell.CellType = CellEnum.TypeOfCell.Empty;
+                break;
+            }
         }
 
         return true;
@@ -386,8 +353,8 @@ public class Bug
 
     private static bool Share(Bug bug)
     {
-        bug.CurrentGenePosition += (int)DestinationCell.CellType + 1;
-        Bug neighbourBug = DestinationCell.LinkedBug;
+        bug.CurrentGenePosition += (int) DestinationCell.CellType + 1;
+        var neighbourBug = DestinationCell.LinkedBug;
         if (neighbourBug != null && neighbourBug.IsFriendBug(bug))
         {
             bug.CurrentGenePosition++;
@@ -400,25 +367,14 @@ public class Bug
 
     private static bool Photosynthesize(Bug bug)
     {
-        bug.CurrentGenePosition += (int)DestinationCell.CellType + 1;
-        Bug neighbourBug = DestinationCell.LinkedBug;
-        if (neighbourBug != null && neighbourBug.IsFriendBug(bug))
-        {
-            bug.CurrentGenePosition++;
-        }
+        bug.CurrentGenePosition += (int) DestinationCell.CellType + 1;
+        var neighbourBug = DestinationCell.LinkedBug;
+        if (neighbourBug != null && neighbourBug.IsFriendBug(bug)) bug.CurrentGenePosition++;
 
-        if (DestinationCell.CellType == CellEnum.TypeOfCell.Mineral)
-        {
-            bug.Health += 5;
-        }
+        if (DestinationCell.CellType == CellEnum.TypeOfCell.Mineral) bug.Health += 5;
 
         return true;
-    } 
-
-    private static bool GenomJump(Bug bug)
-    {
-        bug.CurrentGenePosition = bug.NextGenePosition(bug.Gene.genome[bug.CurrentGenePosition]);
-        return false;
     }
-}
 
+    #endregion
+}
