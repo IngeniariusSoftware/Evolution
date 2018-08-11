f80f234a65e4cca5644ef99b574b9311c4940f3b
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-11 15:52:47 +0500

    Перенос логики генома и его констант в класс Genome

--- Assets/Scripts/Genome.cs
+++ Assets/Scripts/Genome.cs
@@ -2,7 +2,25 @@
 
 public class Genome
 {
-    public int[] genome = new int[Data.LengthGenome];
+    #region Constants
+
+    /// <summary>
+    ///     Количество ячеек в геноме жука
+    /// </summary>
+    public static readonly int LengthGenome = 128;
+
+    #endregion
+
+    private int _currentGenePosition;
+
+    public int CurrentGenePosition
+    {
+        get { return _currentGenePosition; }
+
+        set { _currentGenePosition = value % Genome.LengthGenome; }
+    }
+
+    public int[] genome = new int[LengthGenome];
 
     public int[] GenomeMutate(int countMutation)
     {
@@ -10,17 +28,22 @@ public class Genome
 
         for (int i = 0; i < countMutation; i++)
         {
-            mutateGenome[Data.Rnd.Next(0, mutateGenome.Length)] = Data.Rnd.Next(0, Data.LengthGenome);
+            mutateGenome[Data.Rnd.Next(0, mutateGenome.Length)] = Data.Rnd.Next(0, LengthGenome);
         }
 
         return mutateGenome;
+    }
+
+    public int NextGenePosition(int shift = 1)
+    {
+        return (CurrentGenePosition + shift) % LengthGenome;
     }
 
     public Genome()
     {
         for (int i = 0; i < genome.Length; i++)
         {
-            genome[i] = Data.Rnd.Next(0, Data.LengthGenome);
+            genome[i] = Data.Rnd.Next(0, LengthGenome);
         }
     }
 
