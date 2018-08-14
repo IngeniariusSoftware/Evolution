a15e008ffc85cc61eb02ad92ed6a11f21d17f993
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-14 21:05:58 +0500

    Вынес логику обновления статистики в отдельный скрипт

---
+++ Assets/Scripts/UIScripts/StatisticsMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Класс, отвечающий за логику меню, в котором отображается основная статистика игры
+/// </summary>
+public class StatisticsMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Массив полей текста, на которые выводится информация о текущей статистики игры
+    /// </summary>
+    public Text[] StatisticsText;
+
+    /// <summary>
+    /// Каждый кадр обновляется статистика
+    /// </summary>
+    void Update()
+    {
+        StatisticsText[0].text = "Поколение: " + ControlScript.bugs.GenerationNumber;
+        StatisticsText[1].text = "Шаг: " + Data.CurrentGameStep;
+        StatisticsText[2].text = "Жуков: " + ControlScript.bugs.CountBugs;
+        StatisticsText[3].text = "Мёртв. жуков: " + Data.NumberDeadBugs;
+        StatisticsText[4].text = "Обыч. ягод: " + Data.CurrentCountObjects[(int)CellEnum.TypeOfCell.Berry];
+        StatisticsText[5].text = "Яда: " + Data.CurrentCountObjects[(int)CellEnum.TypeOfCell.Poison];
+        StatisticsText[6].text = "Стен: " + Data.CurrentCountObjects[(int)CellEnum.TypeOfCell.Wall];
+        StatisticsText[7].text = "Минералов: " + Data.CurrentCountObjects[(int)CellEnum.TypeOfCell.Mineral];
+        StatisticsText[8].text = "Минер. ягод: " + Data.CurrentCountObjects[(int)CellEnum.TypeOfCell.MineralBerry];
+        StatisticsText[9].text = "Бамбука: " + Data.CurrentCountObjects[(int)CellEnum.TypeOfCell.Bamboo];
+    }
+}
