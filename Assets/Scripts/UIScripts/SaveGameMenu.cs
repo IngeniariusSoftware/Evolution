dfa0f3330e03ffb752146a731fb0a68beed5c981
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-14 21:07:55 +0500

    Вынес логику работы с меню сохранения в отдельный скрипт

---
+++ Assets/Scripts/UIScripts/SaveGameMenu.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Класс, отвечающий за логику меню сохранения игры
+/// </summary>
+public class SaveGameMenu : MonoBehaviour
+{
+    #region elementsUI
+
+    /// <summary>
+    /// Поле для имени сохранения
+    /// </summary>
+    public InputField SaveNameField;
+
+    /// <summary>
+    /// Кнопка для сохранения игры
+    /// </summary>
+    public Transform SaveButton;
+
+    /// <summary>
+    /// Меню сохранения игры
+    /// </summary>
+    public Transform Menu;
+
+    #endregion
+
+    /// <summary>
+    /// Проверка нужно ли закрыть меню
+    /// </summary>
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Close();
+        }
+    }
+
+    /// <summary>
+    /// Метод, который вызывает сохранения игры
+    /// </summary>
+    public void Save()
+    {
+        Close();
+        SavesManager.SaveGame(SaveNameField.text);
+    }
+
+    /// <summary>
+    /// Метод, для проверки пустоты поля игры
+    /// </summary>
+    public void CheckField()
+    {
+        if (SaveNameField.text != null)
+        {
+            SaveButton.GetComponent<CanvasGroup>().alpha = 1;
+            SaveButton.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+        else
+        {
+            SaveButton.GetComponent<CanvasGroup>().alpha = 0;
+            SaveButton.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+    }
+
+    /// <summary>
+    /// Метод, который открывает меню сохранения игры
+    /// </summary>
+    public void Open()
+    {
+        Menu.GetComponent<CanvasGroup>().alpha = 1;
+        Menu.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        CheckField();
+    }
+
+    /// <summary>
+    /// Метод, который закрывает меню сохранения игры
+    /// </summary>
+    public void Close()
+    {
+        Menu.GetComponent<CanvasGroup>().alpha = 0;
+        Menu.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+}
