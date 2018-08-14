63dbbe31503d5d36d043b7f9366f8b6a6a2feefa
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-14 21:05:32 +0500

    Переименованный скрипт для управления временем прорисовки

---
+++ Assets/Scripts/UIScripts/TimeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Slider = UnityEngine.UI.Slider;
+
+/// <summary>
+/// Класс, отвечающий за регулирование скорости игры
+/// </summary>
+public class TimeManager : MonoBehaviour
+{
+    /// <summary>
+    /// Поле отвечающие за количество шагов отрисовки
+    /// </summary>
+    public static int TimeSpeed = RenderingScript.MaxStepsRendering;
+
+    /// <summary>
+    /// Метод вызывается, когда меняется значение ползунка
+    /// </summary>
+    public void ChangeTimeSped()
+    {
+        TimeSpeed = (int)gameObject.GetComponent<Slider>().value;
+    }
+}
