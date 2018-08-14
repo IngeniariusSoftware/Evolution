42436733b914d1398f84646f2f0be40d47adfd6f
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-08-14 21:08:21 +0500

    Вынес логику переключения режимов отрисовки в отдельный скрипт

---
+++ Assets/Scripts/UIScripts/RenderModeManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Класс для управления режимами отрисовки
+/// </summary>
+public class RenderModeManager : MonoBehaviour
+{
+    /// <summary>
+    /// Текущий режим отрисовки
+    /// </summary>
+    public static bool CurrentRenderingMode = RenderingScript.RenderingMode;
+
+    /// <summary>
+    /// Переключатель режима отрисовки
+    /// </summary>
+    public Toggle NormalViewToggle;
+
+    /// <summary>
+    /// Переключатель режима отрисовки
+    /// </summary>
+    public Toggle EnergyViewToggle;
+
+    /// <summary>
+    /// Метод изменения режима отрисовки, основан на состояниях переключателей
+    /// </summary>
+    public void ChangeRenderingMode()
+    {
+        if (EnergyViewToggle.isOn && NormalViewToggle.isOn)
+        {
+            if (CurrentRenderingMode)
+            {
+                NormalViewToggle.isOn = false;
+            }
+            else
+            {
+                EnergyViewToggle.isOn = false;
+            }
+
+            CurrentRenderingMode = !CurrentRenderingMode;
+        }
+        else
+        {
+            if (!EnergyViewToggle.isOn && !NormalViewToggle.isOn)
+            {
+                if (CurrentRenderingMode)
+                {
+                    EnergyViewToggle.isOn = true;
+                }
+                else
+                {
+                    NormalViewToggle.isOn = true;
+                }
+
+                CurrentRenderingMode = !CurrentRenderingMode;
+            }
+        }
+    }
+}
