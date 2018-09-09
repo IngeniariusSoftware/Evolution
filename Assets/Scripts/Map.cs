7df67ac8c83238bcf7d8020a9c5ec6a52a304a49
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-09-10 02:20:34 +0500

    1) Добавил поддержку листов всех сущностей
    2) Добавил две новые сущности: колючка, солнечный свет
    
    3) Добавил генерацию сущностей в виде больших скоплений, а не равномерно

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using UnityEngine;
 
 public static class Map
@@ -9,12 +9,12 @@ public static class Map
     /// <summary>
     ///     Размер карты по y и по x (количество клеток)   
     /// </summary>
-    public static readonly Coordinates Size = new Coordinates(30, 30);
+    public static readonly Coordinates Size = new Coordinates(40, 80);
 
     /// <summary>
     ///     Процент от общего числа клеток различных объектов на карте 
     /// </summary>
-    public static readonly float[] PercentObjects = { 0, 0.1f, 0.06f, 0.14f, 0.1f, 0, 0.04f, 0 };
+    public static readonly float[] PercentObjects = { 0, 0.1f, 0.1f, 0f, 0.1f, 0, 0.1f, 0.1f, 0.1f, 0 };
 
     /// <summary>
     ///     Общее количетсво клеток на карте
@@ -24,7 +24,7 @@ public static class Map
     /// <summary>
     ///     Количество типов объектов на карте
     /// </summary>
-    public static readonly int[] CountTypeObjects =
+    public static int[] CountTypeObjects =
         {
             (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Empty]),
             (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Berry]),
@@ -33,8 +33,12 @@ public static class Map
             (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Mineral]),
             (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.MineralBerry]),
             (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Bamboo]),
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Sun]),
+            (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Prickle]),
             (int)(AllCellCount * PercentObjects[(int)CellEnum.TypeOfCell.Bug])
         };
+
+    public static List<List<Coordinates>> CellLists = new List<List<Coordinates>>();
 
     #endregion
 
@@ -43,34 +47,65 @@ public static class Map
     /// </summary>
     public static void RefreshMap()
     {
-        for (int i = 0; i < CountTypeObjects.Length; i++)
+        for (int i = 1; i < CountTypeObjects.Length; i++)
         {
-            while (Data.CurrentCountObjects[i] < CountTypeObjects[i])
+            if (Data.CurrentCountObjects[i] < CountTypeObjects[i]
+                && CellLists[(int)CellEnum.TypeOfCell.Empty].Count > 0)
             {
-                if (i == (int)CellEnum.TypeOfCell.Bamboo)
+                Cell checkCell;
+                if (CellLists[i].Count > 0)
                 {
-                    bool find = false;
-                    for (int j = 0; j < 50 && !find; j++)
-                    {
-                        Cell checkCell = FindEmptyCell();
-                        if (CheckRadius(checkCell, CellEnum.TypeOfCell.Bamboo))
-                        {
-                            checkCell.CellType = CellEnum.GetCellType(i);
-                            find = true;
-                        }
-                    }
-
-                    if (!find)
-                    {
-                        FindEmptyCell().CellType = CellEnum.GetCellType(i);
-                    }
+                    Coordinates anotherCell = CellLists[i][Data.Rnd.Next(0, CellLists[i].Count)];
+                    checkCell = Data.WorldMap[anotherCell.Y, anotherCell.X];
                 }
                 else
                 {
-                    FindEmptyCell().CellType = CellEnum.GetCellType(i);
+                    checkCell = FindEmptyCell();
+                    checkCell.CellType = CellEnum.GetCellType(i);
+                }
+
+                while (Data.CurrentCountObjects[i] < CountTypeObjects[i]
+                       && CellLists[(int)CellEnum.TypeOfCell.Empty].Count > 0)
+                {
+                    checkCell = FindEmptyCell(checkCell.Coordinate);
+                    if (checkCell != null)
+                    {
+                        checkCell.CellType = CellEnum.GetCellType(i);
+                    }
+                    else
+                    {
+                        Coordinates anotherCell = CellLists[i][Data.Rnd.Next(0, CellLists[i].Count)];
+                        checkCell = Data.WorldMap[anotherCell.Y, anotherCell.X];
+                    }
                 }
             }
         }
+
+        //for (int i = 0; i < CountTypeObjects.Length; i++)
+        //{
+        //    while (CellLists[(int)CellEnum.TypeOfCell.Empty].Count > 0 && Data.CurrentCountObjects[i] < CountTypeObjects[i])
+        //    {
+        //        if (i == (int)CellEnum.TypeOfCell.Bamboo)
+        //        {
+        //            Cell checkCell = FindEmptyCell();
+        //            checkCell.CellType = CellEnum.TypeOfCell.Bamboo;
+        //            while (Data.Rnd.Next(0, 50) != 0 && CellLists[(int)CellEnum.TypeOfCell.Empty].Count > 0
+        //                                             && Data.CurrentCountObjects[i] < CountTypeObjects[i]
+        //                                             && checkCell != null)
+        //            {
+        //                checkCell = FindEmptyCell(checkCell.Coordinate);
+        //                if (checkCell != null)
+        //                {
+        //                    checkCell.CellType = CellEnum.TypeOfCell.Bamboo;
+        //                }
+        //            }
+        //        }
+        //        else
+        //        {
+        //            FindEmptyCell().CellType = CellEnum.GetCellType(i);
+        //        }
+        //    }
+        //}
     }
 
     public static bool CheckRadius(Cell cell, CellEnum.TypeOfCell cellType)
@@ -89,14 +124,22 @@ public static class Map
 
     public static Cell FindEmptyCell()
     {
-        Coordinates randomPosition;
-        do
-        {
-            randomPosition = Coordinates.RandomCoordinates(Size.Y, Size.X);
-        }
-        while (Data.WorldMap[randomPosition.Y, randomPosition.X].CellType != CellEnum.TypeOfCell.Empty);
+        Coordinates randomCoordinate = CellLists[(int)CellEnum.TypeOfCell.Empty][Data.Rnd.Next(
+            0,
+            CellLists[(int)CellEnum.TypeOfCell.Empty].Count)];
+        return Data.WorldMap[randomCoordinate.Y, randomCoordinate.X];
+    }
 
-        return Data.WorldMap[randomPosition.Y, randomPosition.X];
+    public static Cell FindEmptyCell(Coordinates coordinate)
+    {
+        int i = Data.Rnd.Next(0, 8);
+        Coordinates checkCoordinate = coordinate + Coordinates.CoordinateShift[i];
+        if (Data.WorldMap[checkCoordinate.Y, checkCoordinate.X].CellType == CellEnum.TypeOfCell.Empty)
+        {
+            return Data.WorldMap[checkCoordinate.Y, checkCoordinate.X];
+        }
+
+        return null;
     }
 
     /// <summary>
@@ -104,6 +147,7 @@ public static class Map
     /// </summary>
     public static void CreateMap()
     {
+        InitializeCellLists();
         for (int x = 0; x < Size.X; x++)
         {
             for (int y = 0; y < Size.Y; y++)
@@ -120,6 +164,24 @@ public static class Map
         }
 
         RefreshMap();
+    }
+
+    /// <summary>
+    /// Инициализия листа листов координат всех клеток карты
+    /// </summary>
+    public static void InitializeCellLists()
+    {
+        for (int i = 0; i < CountTypeObjects.Length; i++)
+        {
+            CellLists.Add(new List<Coordinates>());
+        }   
+    }
+
+    public static void UpdateCellList(Cell cell, CellEnum.TypeOfCell newType)
+    {
+        CellLists[(int)cell.CellType].Remove(cell.Coordinate);
+        CellLists[(int)newType].Add(cell.Coordinate);
+        RenderingScript.UpdateTypeCell(cell);
     }
 }
 
