b1ca7298d0696a773bae4aabef6e48ae94859d99
Максим <kyngfy-panda@yandex.ru>
2019-01-02 19:44:13 +0500

    Убрал из даты текущее количество объектов

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -9,32 +9,22 @@ public static class Data
     #region Constants
 
     /// <summary>
-    ///     Переменная для генерации случайный значений
+    /// Переменная для генерации случайный значений
     /// </summary>
     public static readonly Random Rnd = new Random();
 
     /// <summary>
-    ///     Начальное количество жуков на карте
+    /// Начальное количество жуков на карте
     /// </summary>
     public static readonly int BugCount = 20;
 
     /// <summary>
-    ///     Количество заполненных клеток
-    /// </summary>
-    public static int CountFillCell = 0;
-
-    /// <summary>
-    ///     Текущее количество различных объектов на карте
-    /// </summary>
-    public static readonly int[] CurrentCountObjects = new int[Map.CountTypeObjects.Length];
-
-    /// <summary>
-    ///     Текущий ход
+    /// Текущий ход
     /// </summary>
     public static int CurrentGameStep = 0;
 
     /// <summary>
-    ///     Количество мертвых жуков
+    /// Количество мертвых жуков
     /// </summary>
     public static int NumberDeadBugs = 0;
 
